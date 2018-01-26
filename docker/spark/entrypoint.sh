e0d146e06cc52d0e05b29d96632946c95c6caeb9
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2018-01-26 18:21:36 -0500

    Fix paths in spark docker image

--- docker/spark/entrypoint.sh
+++ docker/spark/entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$AWS_ACCESS_KEY" ] && [ "$AWS_SECRET_KEY" ]
 then
     sed -i -e "s~\${aws_access_key}~$AWS_ACCESS_KEY~" \
         -e "s~\${aws_secret_key}~$AWS_SECRET_KEY~" \
-        /root/spark/conf/spark-defaults.conf
+        /opt/spark/conf/spark-defaults.conf
 fi
 
 exec "$@"
